a622575e4d9c05dffcf00a91d375bb0e26d71eb3
Anna Boval <anna.bo1106@gmail.com>
2022-07-21 15:21:02 +0200

    changed some files and get planets on db

--- src/app.js
+++ src/app.js
@@ -8,8 +8,15 @@ app.use(express.json());
 app.use(cors());
 
 // GET THE FULL LIST OF PLANETS
-
-
+app.get('/planets', async (req, res) => {
+  try {
+    const [planets] = await db.promise().query('SELECT * FROM planets');
+    res.send(planets);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('something wrong happened');
+  }
+});
 
 // GET ONE PLANET BY ID
 
