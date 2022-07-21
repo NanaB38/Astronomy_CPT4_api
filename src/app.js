bb8d4d522704c632698802ce6de93e161747b2f4
Anna Boval <anna.bo1106@gmail.com>
2022-07-21 15:50:49 +0200

    CRUD done for planets

--- src/app.js
+++ src/app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const Joi = require('joi');
 const db = require('./db');
-// const connexion = require('./db');
+const connexion = require('./db');
+
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
 // GET THE FULL LIST OF PLANETS
+
 app.get('/planets', async (req, res) => {
   try {
     const [planets] = await db.promise().query('SELECT * FROM planets');
@@ -19,11 +22,62 @@ app.get('/planets', async (req, res) => {
 });
 
 // GET ONE PLANET BY ID
+app.get('/planets/:id', async (req, res) => {
+  const { id } = req.params;
+  connexion
+    .promise()
+    .query('SELECT * FROM planets WHERE id = ?', [id])
+    .then(([results]) => {
+      res.json(results);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send('Error retrieving the planet from the database');
+    });
+});
 
-// TO CREATE A NEW PLANET POST
+// CREATE A NEW PLANET POST
 
-// UPDATE A PLANET POST
+app.post('/planets', async (req, res) => {
+  try {
+    const { name, picture, details } = req.body;
+    const { error: validationErrors } = Joi.object({
+      name: Joi.string().max(255).required(),
+      picture: Joi.string().max(255).required(),
+      details: Joi.string().max(255).required(),
+    }).validate({ name, picture, details }, { abortEarly: false });
 
-// DELETE A POST
+    if (validationErrors) {
+      return res.status(422).json({ errors: validationErrors.details });
+    }
+    const [{ insertId }] = await db
+      .promise()
+      .query('INSERT INTO planets (name, picture, details) VALUES (?, ?, ?)', [
+        name,
+        picture,
+        details,
+      ]);
+    res.status(201).send({ id: insertId, name, picture, details });
+  } catch (err) {
+    console.error(err);
+    res.sendStatus(500);
+  }
+});
+
+// DELETE A PLANET POST
+
+app.delete('/planets/:id', async (req, res) => {
+  const { id } = req.params;
+  connexion
+    .promise()
+    .query('DELETE FROM planets WHERE id = ?', [id])
+    .then(() => {
+      res.status(200).send('Post has been successfully deleted');
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send('Error deleting your planet post');
+    });
+});
 
 module.exports.app = app;
